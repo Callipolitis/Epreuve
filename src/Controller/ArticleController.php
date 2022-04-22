a7c49d749b8a02a91672aee253474ee9cdf8c93c
Callipolitis <yanis.callipolitis@gmail.com>
2022-04-22 11:56:59 +0200

    category ++++

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -20,16 +20,6 @@ class ArticleController extends AbstractController
      */
     public function index(ArticleRepository $articleRepository): Response
     {
-        dd(1);
-        return $this->render('article/index.html.twig', [
-            'articles' => $articleRepository->findAll(),
-        ]);
-    }
-    /**
-     * @Route("/articles", name="app_article_index", methods={"GET"})
-     */
-    public function indexs(ArticleRepository $articleRepository): Response
-    {
         return $this->render('article/index.html.twig', [
             'articles' => $articleRepository->findAll(),
         ]);
